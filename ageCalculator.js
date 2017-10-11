be77b607c97bb18e1b17588a8e97b2da4f225d38
MattEmond <memond36@gmail.com>
2017-10-11 16:02:09 +0000

    Add age calculator exercise

---
+++ ageCalculator.js
@@ -0,0 +1,7 @@
+function ageCalculator(name, yearOfBirth, currentYear) {
+  var age = currentYear - yearOfBirth
+  return name + ' is ' + age + ' years old.'
+}
+
+ageCalculator("Miranda", 1983, 2015);
+ageCalculator("Ferdinand", 1988, 2015);
